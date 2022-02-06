2f99f12641ec86983868c1034dbe3d496364d681
elisad5791 <elisad5791@mail.ru>
2022-02-06 15:40:24 +0500

    Fix Github Actions

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
